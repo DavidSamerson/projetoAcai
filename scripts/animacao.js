7c17911afa81c3e693be117ae98c3b70981eb285
DavidSamerson <samerson10@gmail.com>
2017-08-20 17:28:21 -0300

    caralho do link ajeitado

---
+++ scripts/animacao.js
@@ -0,0 +1,21 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+})(jQuery); // End of use strict
